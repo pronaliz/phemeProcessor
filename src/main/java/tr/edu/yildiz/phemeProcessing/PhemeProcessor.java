1ef85658b2902fac986bc5b4fc6a2598e1e4c011
Jehad Baeth <jihadbaeth@gmail.com>
2018-12-16 11:31:20 +0300

    tweaking the calculations Trial #9875646524894 -_-

--- src/main/java/tr/edu/yildiz/phemeProcessing/PhemeProcessor.java
+++ src/main/java/tr/edu/yildiz/phemeProcessing/PhemeProcessor.java
@@ -21,7 +21,16 @@ public class PhemeProcessor {
 
 
     public static void main(String[] args) {
-
+        PrintWriter pw = null;
+        try {
+            pw = new PrintWriter(new File("C:\\Users\\jehad\\Desktop\\phemeProcessor\\phemeDataset/output.csv"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        StringBuilder builder = new StringBuilder();
+        String ColumnNamesList = "Thread ID,Distance From Positivity, Thread Certainity";
+        // No need give the headers Like: id, Name on builder.append
+        builder.append(ColumnNamesList + "\n");
         final PhemeProcessor phemeProcessor = new PhemeProcessor();
         ObjectMapper objectMapper = new ObjectMapper();
         String datasetPath = "C:\\Users\\jehad\\Desktop\\phemeProcessor\\phemeDataset/";
@@ -37,29 +46,63 @@ public class PhemeProcessor {
                 if (line.startsWith("#")) continue;
                 annotationsList.add(objectMapper.readValue(line, Annotations.class));
             }
-            logger.info("Finished importing main annotations file");
-            logger.info("number of annotations imported: " + annotationsList.size());
-            logger.info("First annotation imported is: " + annotationsList.get(0).toString());
-            logger.info("Last annotation imported: " + annotationsList.get(annotationsList.size() - 1).toString());
-            Map<Pair<String, String>, ReplyAnnotation> replyAnnotationMap = phemeProcessor.getAllAnnotations(datasetPath);
+            Map<String, Annotations> annotationsMap = new HashMap<>();
+            for (Annotations annotation : annotationsList) {
+                annotationsMap.put(annotation.getThreadid(), annotation);
 
-            //test
-            String tweetPath = datasetPath + "threads/en/" + annotationsList.get(0).getEvent() + "/" + annotationsList.get(0).getThreadid();
-            File mainTweetJSONFile = new File(tweetPath + "/" + SOURCE + "/" + annotationsList.get(0).getThreadid() + ".json");
-            Tweet mainTweet = phemeProcessor.getTweet(mainTweetJSONFile);
-            List<Tweet> reactions = phemeProcessor.getReactions(annotationsList.get(0), datasetPath);
+            }
+            logger.debug("Finished importing main annotations file");
+            logger.debug("number of annotations imported: " + annotationsList.size());
 
-            phemeProcessor.getPopularity(mainTweet);
-            List<Double> popularity = reactions.stream().map(tweet -> {
-                ReplyAnnotation replyAnnotation = replyAnnotationMap.get(Pair.of(tweet.getInReplyToStatusIdStr(), tweet.getIdStr()));
-                if (replyAnnotation.getResponsetypeVsSource().equals("disagreed")) {
-                    return -Math.abs(phemeProcessor.getPopularity(tweet));
+            for (Annotations annotation : annotationsList) {
+                logger.debug("First annotation imported is: " + annotation.toString());
+                Map<Pair<String, String>, ReplyAnnotation> replyAnnotationMap = phemeProcessor.getAllAnnotations(datasetPath);
+
+                //test
+                String tweetPath = datasetPath + "threads/en/" + annotation.getEvent() + "/" + annotation.getThreadid();
+                File mainTweetJSONFile = new File(tweetPath + "/" + SOURCE + "/" + annotation.getThreadid() + ".json");
+                Tweet mainTweet = phemeProcessor.getTweet(mainTweetJSONFile);
+                List<Tweet> reactions = phemeProcessor.getReactions(annotation, datasetPath);
+
+                double mainTweetPopularity = phemeProcessor.getPopularity(mainTweet);
+                List<Double> popularity = reactions.stream().filter(tweet -> {
+                    ReplyAnnotation replyAnnotation = replyAnnotationMap.get(Pair.of(mainTweet.getIdStr(), tweet.getIdStr()));
+                    return replyAnnotation != null && replyAnnotation.getResponsetypeVsSource().equals("comment");
+                }).map(tweet -> {
+                    ReplyAnnotation replyAnnotation = replyAnnotationMap.get(Pair.of(mainTweet.getIdStr(), tweet.getIdStr()));
+
+                    if (replyAnnotation != null && (replyAnnotation.getResponsetypeVsSource().equals("disagreed") ||
+                            replyAnnotation.getResponsetypeVsSource().equals("appeal-for-more-information"))) {
+                        return (-1 * Math.abs(phemeProcessor.getPopularity(tweet)));
+                    } else {
+                        return Math.abs(phemeProcessor.getPopularity(tweet));
+                    }
+
+
+                }).collect(Collectors.toList());
+                logger.debug("List of popularity Size: " + popularity.size());
+                popularity.stream().forEach(logger::debug);
+                Double cumulativeCredibility = 0.0;
+                cumulativeCredibility = popularity.stream().mapToDouble(Math::abs).sum();
+                cumulativeCredibility += mainTweetPopularity;
+                double distanceFromPosititvity = mainTweetPopularity / Math.abs(cumulativeCredibility);
+                for (double creibility : popularity) {
+                    distanceFromPosititvity = distanceFromPosititvity + (creibility / Math.abs(cumulativeCredibility));
                 }
-                return Math.abs(phemeProcessor.getPopularity(tweet));
+                builder.append(mainTweet.getIdStr() + ",");
+                builder.append(distanceFromPosititvity + ",");
+                builder.append(annotationsMap.get(mainTweet.getIdStr()).getCertainty());
+                builder.append('\n');
 
-            }).collect(Collectors.toList());
-            logger.info("List of popularity Size: " + popularity.size());
-            popularity.stream().forEach(logger::info);
+                logger.info("#################################################");
+                logger.info("DistanceFromPositivity equals: " + distanceFromPosititvity);
+                logger.info("Main Tweet Certainty: " + annotationsMap.get(mainTweet.getIdStr()).getCertainty());
+                logger.info("#################################################");
+
+            }
+            pw.write(builder.toString());
+            pw.close();
+
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -90,8 +133,8 @@ public class PhemeProcessor {
                 tweetFileString = tweetFileString.concat(line);
             }
             tweet = objectMapper.readValue(tweetFileString, Tweet.class);
-            logger.info("Finished importing tweet file");
-            logger.info("Tweet JSON file " + tweet.toString());
+            logger.debug("Finished importing tweet file");
+            logger.debug("Tweet JSON file " + tweet.toString());
 
 
         } catch (FileNotFoundException e) {
@@ -99,8 +142,8 @@ public class PhemeProcessor {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        logger.info("Number of Favs: " + tweet.getFavoriteCount());
-        logger.info("Number of Retweets:" + tweet.getRetweetCount());
+        logger.debug("Number of Favs: " + tweet.getFavoriteCount());
+        logger.debug("Number of Retweets:" + tweet.getRetweetCount());
         return tweet;
     }
 
@@ -109,11 +152,11 @@ public class PhemeProcessor {
         File reactionsFolder = new File(reactionPath + REACTION);
         List<Tweet> reactions = new ArrayList<Tweet>();
         for (File tweetReaction : reactionsFolder.listFiles()) {
-            //logger.info("List of Reaction JSON Tweet files");
-            //logger.info(reactionsFolder.list());
+            logger.debug("List of Reaction JSON Tweet files");
+            logger.debug(reactionsFolder.list());
             reactions.add(getTweet(tweetReaction));
         }
-        logger.info("Number of Reactions: " + reactions.size());
+        logger.debug("Number of Reactions: " + reactions.size());
         return reactions;
     }
 
@@ -126,18 +169,21 @@ public class PhemeProcessor {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
 
             String line = null;
-            annotationsMap = new HashMap<Pair<String, String>, ReplyAnnotation>();
+            annotationsMap = new HashMap<>();
             while ((line = bufferedReader.readLine()) != null) {
                 if (line.startsWith("#")) continue;
                 annotationsMap.put(Pair.of((objectMapper.readValue(line, ReplyAnnotation.class)).getThreadid(),
                         (objectMapper.readValue(line, Annotations.class)).getTweetid()), (objectMapper.readValue(line, ReplyAnnotation.class)));
             }
-            logger.info("Finished importing all annotations file");
-            logger.info("number of annotations imported: " + annotationsMap.size());
+            logger.debug("Finished importing all annotations file");
+            logger.debug("number of annotations imported: " + annotationsMap.size());
         } catch (FileNotFoundException e) {
+            logger.fatal("an Error has occured " + e.getMessage());
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+            logger.fatal("an Error has occured " + e.getMessage());
+
         } finally {
 
         }
